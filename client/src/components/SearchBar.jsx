c02e6bcd6b7ab0145ecc96be4df576608158b564
Anvay Panguluri <apangul@amazon.com>
2025-07-26 14:54:43 -0700

    rendered response on frontend

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const SearchBar = () => {
   const [input, setInput] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleKeyPress = async (event) => {
     if (event.key === "Enter") {
@@ -9,23 +10,32 @@ const SearchBar = () => {
         console.log("User submitted:", input);
         // Make API call to 127.0.0.1/analyze?amazon_product_url=input
         const response = await fetch(`http://127.0.0.1:8000/analyze?amazon_product_url=${encodeURIComponent(input)}`)
+        const data = await response.json();
 
-        console.log("API response:", response);
+        console.log("API response:", data);
+        setMessage(data.analysis || JSON.stringify(data));
       } catch (error) {
-        console.error("API error:", error);
+        console.error("API error: ", error);
       }
     }
   };
 
   return (
-    <input
-      type="text"
-      placeholder="Search or paste product link..."
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
-      onKeyDown={handleKeyPress}
-      className="search-input"
-    />
+    <div>
+      <input
+        type="text"
+        placeholder="Search or paste product link..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={handleKeyPress}
+        className="search-input"
+      />
+      {message && (
+        <div className="api-message" style={{ marginTop: "1em"}}> 
+          {message}
+        </div>
+      )}
+    </div>
   );
 };
 
